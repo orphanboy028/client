4df2b57afec9dac4e7a648306896a8fc5ad39319
sanjay <orphanboy028@gmail.com>
2023-04-13 06:56:26 +0530

    created product page

--- components/ProductComponent/Product/ProductList.js
+++ components/ProductComponent/Product/ProductList.js
@@ -10,6 +10,8 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { locationList } from "../../FormData/location.js";
 
+import productImage from "../../../public/product-feature-imges/user-64318e457993fb3620054341-1680969454724.png";
+
 export default function ProductList() {
   return (
     <>
@@ -69,6 +71,42 @@ export default function ProductList() {
         </div>
       </div>
       {/* Search Bar section End */}
+
+      <div className={style.ProductList_main_Container}>
+        <div className={style.ProductList_left_sideBar}>Left Bar</div>
+        <div className={style.product_centerBox}>
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((product, i) => {
+            return (
+              <>
+                <div className={style.card_container}>
+                  <div className={style.productImage_Box}>
+                    <Image src={productImage} width={100} alt="Product-Image" />
+                  </div>
+                  <div className={style.product_Card_Body}>
+                    <div className={style.Product_titleBox}>
+                      Turning Fixture
+                    </div>
+                    <div className={style.product_descreptionBox}>
+                      <p>
+                        In publishing and graphic design, Lorem ipsum is a
+                        placeholder text
+                      </p>
+                    </div>
+
+                    <div className={style.product_priceBox}>
+                      <div className={style.price_TextBox}>5550</div>
+                      <div className={style.card_Btn_BOX}>
+                        <button>Enquery</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+        <div className={style.ProductList_rightBox}>right box</div>
+      </div>
     </>
   );
 }
